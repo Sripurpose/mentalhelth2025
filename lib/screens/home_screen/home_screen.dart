1ff265a96318c38e943ec6261fa79f0be7f2159d
sarath.p@purposecodes.com <sarath.p@gjglobalsoft.com>
2025-12-09 14:13:02 +0530

    09-12-2025  -- bug fix

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -327,7 +327,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void checkVersionUpdate() async {
     final String androidUpdateUrl = "https://play.google.com/store/apps/details?id=com.numuapp.numuapp";
-    final String iosUpdateUrl = "https://apps.apple.com/app/id6736739491"; // Replace with your iOS App Store link
+    final String iosUpdateUrl = "https://apps.apple.com/app/id6746407087"; // Replace with your iOS App Store link
     final prefs = await SharedPreferences.getInstance();
     final lastSkippedTimestamp = prefs.getInt('lastSkippedTimestamp');
     final currentTime = DateTime.now().millisecondsSinceEpoch;
